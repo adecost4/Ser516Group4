b67376514cf6d26134deb316283767eff751fcd9
rwalter8 <rwalter8@asu.edu>
2026-02-13 00:03:42 -0700

    slight correction to ClassParser + add ide folders to vscode

--- src/main/java/com/cohesion/classparsing/ClassParser.java
+++ src/main/java/com/cohesion/classparsing/ClassParser.java
@@ -4,7 +4,7 @@ public class ClassParser {
     // given a class, count all of the methods (private, public, constructors)
     public int countClassMethods(Class<?> currClass) {
         int methodCount = currClass.getDeclaredMethods().length;
-        methodCount += currClass.getMethods().length;
+        methodCount += currClass.getConstructors().length;
         return methodCount;
     }
 }
