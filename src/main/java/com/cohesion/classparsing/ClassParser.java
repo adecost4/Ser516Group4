5c9d5783916380ebc521a32047a48834a08905bf
rwalter8 <rwalter8@asu.edu>
2026-02-13 00:07:42 -0700

    Added method to count fields

--- src/main/java/com/cohesion/classparsing/ClassParser.java
+++ src/main/java/com/cohesion/classparsing/ClassParser.java
@@ -7,4 +7,10 @@ public class ClassParser {
         methodCount += currClass.getConstructors().length;
         return methodCount;
     }
+
+    // given a class, count all of the fields (private, public)
+    public int countClassFields(Class<?> currClass) {
+        int fieldCount = currClass.getDeclaredFields().length;
+        return fieldCount;
+    }
 }
