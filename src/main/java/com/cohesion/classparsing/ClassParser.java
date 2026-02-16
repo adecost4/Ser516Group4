604edfc298ca28a0a0cfe9cf93fb72e7d9cb69c7
rwalter8 <rwalter8@asu.edu>
2026-02-16 00:24:23 -0700

    Initial commit for US3

---
+++ src/main/java/com/cohesion/classparsing/ClassParser.java
@@ -0,0 +1,7 @@
+package com.cohesion.classparsing;
+
+public class ClassParser {
+    // TODO: implement method to find what other methods are called by a class (AST parsing)?
+
+    // TODO: implement method to find what other fields are called by a class (AST parsing)?
+}
