710cd336fcb3dffdf9acda374ad407e26773e7c0
rwalter8 <rwalter8@asu.edu>
2026-02-17 13:05:00 -0700

    renamed class parser for clarity (US3)

--- src/main/java/com/cohesion/classparsing/LCOM4ClassParser.java
+++ src/main/java/com/cohesion/classparsing/LCOM4ClassParser.java
@@ -1,6 +1,6 @@
 package com.cohesion.classparsing;
 
-public class ClassParser {
+public class LCOM4ClassParser {
     // TODO: implement method to find what other methods are called by a class (AST parsing)?
 
     // TODO: implement method to find what other fields are called by a class (AST parsing)?
