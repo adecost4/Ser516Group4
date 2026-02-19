bc9ac26db04f4282bf5f455ef4e9bda70fdf851f
rwalter8 <rwalter8@asu.edu>
2026-02-18 22:19:59 -0700

    another initial commit for us3

---
+++ src/main/java/com/cohesion/classparsing/LCOMHSClassParser.java
@@ -0,0 +1,10 @@
+package com.cohesion.classparsing;
+
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+
+public class LCOMHSClassParser {
+    public int countSumOfMethodsAccessingFields(ClassOrInterfaceDeclaration classDecl) {
+        // Placeholder implementation
+        return 0;
+    }
+}
