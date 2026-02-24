7e65472f6bca4d8dda28dbcfa3eff6934be3ff44
Aloyson <aloyson9@gmail.com>
2026-02-23 21:02:43 -0700

    Task 55

--- src/main/java/com/cohesion/Main.java
+++ src/main/java/com/cohesion/Main.java
@@ -6,10 +6,13 @@ import java.util.List;
 
 import com.cohesion.classes.MFResult;
 import com.cohesion.classparsing.LCOMHSClassParser;
+import com.cohesion.metrics.MetricsServer;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        MetricsServer metricsServer = new MetricsServer();
+        metricsServer.start(8080);
         // System.out.println(
         //         "if you wanna test the scanner run mvn clean package and \r\n" + //
         //                 "java -cp target/Ser516Group4-1.0-SNAPSHOT.jar com.cohesion.Main <path to your java project>");
@@ -21,10 +24,6 @@ public class Main {
 
         ProjectScanner scanner = new ProjectScanner();
         List<Path> javaFiles = scanner.scanProject(args[0]);
-
-        System.out.println("Found " + javaFiles.size() + " Java files:");
-
-        javaFiles.forEach(System.out::println);
         LCOMHSClassParser parser = new LCOMHSClassParser();
         List<MetricRecord> records = new ArrayList<>();
 
@@ -35,21 +34,11 @@ public class Main {
 
             long timestamp=System.currentTimeMillis()/1000;
             // Compute LCOMHS
-            lcomhs = LCOMHSCalculator.computeLcomhs(r.getM(), r.getF(), r.getSUMMF());   
-            records.add(new MetricRecord(r.getPackageName(), r.getClassName(), "LCOMHS", lcomhs, timestamp));       
-                System.out.println("File: " + file.getFileName());
-                System.out.println("Class: " + r.getClassName());
-                System.out.println("M (methods+ctors): " + r.getM());
-                System.out.println("F (instance fields): " + r.getF());
-                System.out.println("MF: " + r.getSUMMF());
-                System.out.printf("LCOMHS=%.6f%n", lcomhs);
-                System.out.println("----------------------------------");
+            lcomhs = LCOMHSCalculator.computeLcomhs(r.getM(), r.getF(), r.getSUMMF());
+            MetricsServer.LCOMHS_GAUGE
+            .labels(r.getClassName(), r.getPackageName(), "LCOMHS")
+            .set(lcomhs);
             }
         }
-        System.out.println("\n=== JSON Output ===");
-        System.out.println(MetricFormatter.toJson(records));
-
-        System.out.println("\n=== Prometheus Output ===");
-        System.out.println(MetricFormatter.toPrometheus(records));
     }
 }
