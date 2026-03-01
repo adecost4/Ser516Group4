e31a616845895efcd7deb276698403a6328089aa
rwalter8 <rwalter8@asu.edu>
2026-03-01 00:35:06 -0700

    Task 45

--- src/main/java/com/leadtime/LeadTimeCalculator.java
+++ src/main/java/com/leadtime/LeadTimeCalculator.java
@@ -1,9 +1,42 @@
 package com.leadtime;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.net.http.HttpResponse;
+import java.time.OffsetDateTime;
 
 public class LeadTimeCalculator {
     public static int calculateLeadTime(HttpResponse<String> userStoryHTTP) {
-        return 44;
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+
+        String body = userStoryHTTP.body();
+
+        UserStory story = null;
+        try {
+            story = mapper.readValue(body, UserStory.class);
+        }
+        catch (Exception e) {
+            System.out.println("Error reading user story JSON: " + body);
+            e.printStackTrace();
+        }
+
+        if (story == null || !story.isClosed) {
+            return -1;
+        } else {
+            return (int) java.time.Duration.between(story.createdDate, story.finishedDate).toDays();
+        }
+    }
+
+    private static class UserStory {
+        @JsonProperty("created_date")
+        OffsetDateTime createdDate;
+
+        @JsonProperty("finished_date")
+        OffsetDateTime finishedDate;
+
+        @JsonProperty("is_closed")
+        boolean isClosed;
     }
 }
