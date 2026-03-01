22586474cec5248a4d259717aff11330e52aa257
rwalter8 <rwalter8@asu.edu>
2026-02-28 23:33:57 -0700

    Task 45: Tests for Test Driven Development

---
+++ src/main/java/com/leadtime/LeadTimeCalculator.java
@@ -0,0 +1,9 @@
+package com.leadtime;
+
+import java.net.http.HttpResponse;
+
+public class LeadTimeCalculator {
+    public static int calculateLeadTime(HttpResponse<String> userStoryHTTP) {
+        return 44;
+    }
+}
